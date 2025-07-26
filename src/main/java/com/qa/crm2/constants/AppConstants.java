38540050bee7facc5bc1e44014987105f67f7043
Praval Mishra <pravalmishra@LP-000873.local>
2025-07-26 09:44:05 +0530

    test

--- src/main/java/com/qa/crm2/constants/AppConstants.java
+++ src/main/java/com/qa/crm2/constants/AppConstants.java
@@ -5,12 +5,12 @@ import java.util.List;
 
 public class AppConstants {
 
-	public static final String LOGIN_PAGE_TITLE = "Account Login1";
+	 public static final String LOGIN_PAGE_TITLE = "Account Login1";
 
-	public static final String ACCOUNTS_PAGE_TITLE = "My Account";
-	public static final String ADDTOCART_PAGE_TITLE = "Shopping Cart";
+	 public static final String ACCOUNTS_PAGE_TITLE = "My Account";
+	 public static final String ADDTOCART_PAGE_TITLE = "Shopping Cart";
 
-	public static final String LOGIN_PAGE_URL_FRACTION = "route=account/login";
+	 public static final String LOGIN_PAGE_URL_FRACTION = "route=account/login";
 	public static final String ACC_PAGE_URL_FRACTION = "route=account/account";
 
 	public static final String USER_REGISTER_SUCCESS_MESSG = "Your Account Has Been Created!";
